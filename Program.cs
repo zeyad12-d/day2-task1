b087aa2efed4608e0e0c56bb3591b3c2830d70cd
zeyad12-d <zeadyasser054@gmail.com>
2024-10-26 06:56:15 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Dynamic;
+
+namespace day2_task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int num3;
+            Console.WriteLine("please enter  num1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter num2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter num3");
+            num3= Convert.ToInt32(Console.ReadLine());
+            int largest = num1;
+            if (largest > num2)
+            {
+                largest = num2;
+
+            }
+            if (largest < num3)
+            {
+                largest = num3;
+
+            }
+            Console.WriteLine($" the laragest number is !{ largest}" );
+
+                
+
+
+        }
+    }
+}
